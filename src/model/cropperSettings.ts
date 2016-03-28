a37e5f78c3f14935efa6a8f1687f9e090eb6a7f1
cstefanache <cornel.stefanache@gmail.com>
2016-03-28 08:56:31 +0200

    added first version for the Angular2 image cropper

---
+++ src/model/cropperSettings.ts
@@ -0,0 +1,16 @@
+export class CropperSettings {
+
+  canvasWidth: number = 300;
+  canvasHeight: number = 300;
+
+  width: number = 200;
+  height: number = 200;
+
+  croppedWidth: number = 100;
+  croppedHeight: number = 100;
+
+
+
+
+
+}
